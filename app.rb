69119ec4920076c12c3faaaa5c4d6271fc521b85
ziesop20 <sincerelysophie@hotmail.com>
2018-07-31 15:41:25 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-end
+  get '/' do 
+    "Hello, World!"
+  end
+
+  get '/name' do 
+    "My name is Sophie"
+  end 
+  
+  get '/hometown' do 
+    "My hometown is Prior Lake, MN"
+  end 
+  
+  get '/favorite-song' do 
+    "My favorite song is Fly Me to the Moon"
+  end 
+end 
