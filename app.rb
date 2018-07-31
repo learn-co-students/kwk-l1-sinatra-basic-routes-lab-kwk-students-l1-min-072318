c1231142ee7d8ac03bc4a5a635036ad05ac47688
KaumudiMummadi <kaumudi.mummadi@gmail.com>
2018-07-31 16:12:58 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do 
+    "Hello, World!"
 end
+
+  get '/name' do 
+    "My name is Kaumudi"
+  end 
+  
+  get '/hometown' do 
+    "My hometown is Rosemount"
+  end 
+  
+  get '/favorite-song' do 
+    "My favorite song is City of Stars"
+  end 
+end 
